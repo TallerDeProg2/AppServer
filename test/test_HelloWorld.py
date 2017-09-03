43a0f5882ad6445926f34c9f792ad9c6d984f407
aBibiloniLombardi <abibiloni@fi.uba.ar>
2017-09-03 16:57:21 -0300

    Agrego carpeta src y test + HolaMundo.py y test

---
+++ test/test_HelloWorld.py
@@ -0,0 +1,18 @@
+import unittest
+
+import sys
+sys.path.append('../src')
+import helloWorld
+
+class TestHelloWorld(unittest.TestCase):
+
+    def test_ReturnIsCorrect(self):
+        message = helloWorld.helloWord()
+        self.assertEqual(message, "Hello world!")
+
+    def test_Split(self):
+        message = helloWorld.helloWord()
+        self.assertEqual(message[0], "H")
+        
+if __name__ == '__main__':
+	unittest.main()
