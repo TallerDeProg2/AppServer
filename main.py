7045961ec56d0f6ede7f09df517a76d2990ff619
aBibiloniLombardi <abibiloni@fi.uba.ar>
2017-09-04 18:03:55 -0300

    cambio el main

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-PORT = 8000
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-
 if __name__ == "__main__":
-	app.run(port = PORT, debug=True)
+	app.run( debug=True)
