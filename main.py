033c99ff1eaf40711f3a8aa60bc781e680d3fb8a
AyeTheBest <abibiloni@fi.uba.ar>
2017-09-06 17:18:08 -0300

    primero paso con Doxygen

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+"""
+Main.py
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+	""" 
+	[Main] Comentario de funcion hola mundo
+	"""
     return "Hello World!"
 
 if __name__ == "__main__":
+	"""
+	Main comentario
+	"""
 	app.run( debug=True)
--- src/HelloWorld.py
+++ src/HelloWorld.py
@@ -1,2 +1,9 @@
+"""@package src
+
+Algo despues del package
+"""
 def helloWorld():
+	"""
+	Comentario de la funcion hola mundo.
+    """
 	return "Hello world!"
