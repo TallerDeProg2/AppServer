d9d8ed8ae2ce3381898d3e732f54e0dae5babf87
sofiamorseletto <sofimorseletto@hotmail.com>
2017-12-10 23:27:08 -0300

    Saco ref a la hora de modificar card y car

--- src/main/edit.py
+++ src/main/edit.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 
 class Edit(Resource):
-    def put(self, id, endpoint, schema):
+    def put(self, id, endpoint, schema, type=None):
         """Permite modificar"""
         token = request.headers['token']
         if not gm.validate_token(token, id):
@@ -21,6 +21,18 @@ class Edit(Resource):
         if not gm.validate_args(schema, content):
             abort(400)
 
+        if type == 'user':
+            content = self.add_ref(endpoint, content)
+
+        try:
+            r = requests.put(endpoint, json=content, headers={'token': 'superservercito-token'})
+            r.raise_for_status()
+        except requests.exceptions.HTTPError:
+            logging.error('Conexión con el Shared dio error en put: ' + repr(r.status_code))
+            abort(r.status_code)
+        return r.json()
+
+    def add_ref(self, endpoint, content):
         try:
             r = requests.get(endpoint, headers={'token': 'superservercito-token'})
             r.raise_for_status()
@@ -29,13 +41,5 @@ class Edit(Resource):
             abort(r.status_code)
 
         content['_ref'] = r.json()['user']['_ref']
-
-        try:
-            r = requests.put(endpoint, json=content, headers={'token': 'superservercito-token'})
-            r.raise_for_status()
-        except requests.exceptions.HTTPError:
-            logging.error('Conexión con el Shared dio error en put: ' + repr(r.status_code))
-            abort(r.status_code)
-
-        return r.json()
+        return content
 
